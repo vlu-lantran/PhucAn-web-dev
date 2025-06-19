c8194899f05b782140f5103b5fa31ffa07309749
minhquan1627 <minhquan1627@gmail.com>
2025-06-20 02:14:36 +0700

    fix: cấu hình lại API URL trong client-customer để hoạt động trong Docker

--- client-customer/src/App.js
+++ client-customer/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <MyProvider>
-        <BrowserRouter >
+        <BrowserRouter basename="/">
           <Main />
         </BrowserRouter>
       </MyProvider>
